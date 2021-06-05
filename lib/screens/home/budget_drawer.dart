179afb00e5d51554e86041881c9740252a2ae9a0
Afuza <theafuza@gmail.com>
2021-06-05 19:29:07 +0700

    Created layout for Home budget drawer
    Displays current budget (dummy) and options to set budget, add expenses, and add income.

--- lib/screens/home/budget_drawer.dart
+++ lib/screens/home/budget_drawer.dart
@@ -1,16 +1,61 @@
 import "package:flutter/material.dart";
 
 class BudgetDrawer extends StatelessWidget {
+  final height = AppBar().preferredSize.height;
+  double space = 35;
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        child: Center(
-          child: Text(
-            "Set budget / Add expenses / Add income",
-            style: TextStyle(color: Colors.black, fontSize: 15),
-          ),
-        ),
+      child: SafeArea(
+        child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 0, vertical: height),
+            child: Column(
+              children: [
+                Text("Current Budget",
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                SizedBox(height: space),
+                Text("\$400", style: TextStyle(fontSize: 19)),
+                SizedBox(height: space),
+                TextButton(
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.brown[500]),
+                      minimumSize:
+                          MaterialStateProperty.all<Size>(Size(150, 40))),
+                  onPressed: () {},
+                  child: Text("Set Budget"),
+                ),
+                SizedBox(height: space),
+                TextButton(
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red),
+                      minimumSize:
+                          MaterialStateProperty.all<Size>(Size(150, 40))),
+                  onPressed: () {},
+                  child: Text("Add Expenses"),
+                ),
+                SizedBox(height: space),
+                TextButton(
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green),
+                      minimumSize:
+                          MaterialStateProperty.all<Size>(Size(150, 40))),
+                  onPressed: () {},
+                  child: Text("Add Income"),
+                ),
+                SizedBox(height: space),
+              ],
+            )),
       ),
     );
   }
