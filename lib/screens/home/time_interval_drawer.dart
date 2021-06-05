7b055bc7a4fe518d5f8dd2f0aaaa5a59317cc6b7
Afuza <theafuza@gmail.com>
2021-06-05 19:29:38 +0700

    Created layout displaying buttons to sort data by
    Can sort by day, week, month, quarter, and year.

--- lib/screens/home/time_interval_drawer.dart
+++ lib/screens/home/time_interval_drawer.dart
@@ -1,17 +1,76 @@
 import "package:flutter/material.dart";
 
 class TimeIntervalDrawer extends StatelessWidget {
+  final height = AppBar().preferredSize.height;
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        child: Center(
-          child: Text(
-            "Sort by Time",
-            style: TextStyle(color: Colors.black, fontSize: 15),
-          ),
-        ),
-      ),
-    );
+        child: SafeArea(
+            child: Container(
+                padding: EdgeInsets.symmetric(vertical: height),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Sort Budget Data By",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextButton(
+                        style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.brown[500]),
+                            minimumSize:
+                                MaterialStateProperty.all<Size>(Size(150, 40))),
+                        onPressed: () {},
+                        child: Text("Day"),
+                      ),
+                      TextButton(
+                        style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.brown[500]),
+                            minimumSize:
+                                MaterialStateProperty.all<Size>(Size(150, 40))),
+                        onPressed: () {},
+                        child: Text("Week"),
+                      ),
+                      TextButton(
+                        style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.brown[500]),
+                            minimumSize:
+                                MaterialStateProperty.all<Size>(Size(150, 40))),
+                        onPressed: () {},
+                        child: Text("Month"),
+                      ),
+                      TextButton(
+                        style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.brown[500]),
+                            minimumSize:
+                                MaterialStateProperty.all<Size>(Size(150, 40))),
+                        onPressed: () {},
+                        child: Text("Quarter"),
+                      ),
+                      TextButton(
+                        style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.brown[500]),
+                            minimumSize:
+                                MaterialStateProperty.all<Size>(Size(150, 40))),
+                        onPressed: () {},
+                        child: Text("Year"),
+                      ),
+                    ]))));
   }
 }
