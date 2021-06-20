d28d9fb629789e85bbbd135d691012c5123e03b1
Afuza <theafuza@gmail.com>
2021-06-20 17:04:14 +0700

    Created constants file. Set text input decoration theme

---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+const textInputDecoration = InputDecoration(
+    hintText: "Enter value",
+    fillColor: Colors.white,
+    filled: true,
+    enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white, width: 2)),
+    focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.brown, width: 2)));
